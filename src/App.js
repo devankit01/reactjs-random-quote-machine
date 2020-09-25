4dc57c581a9a9e96cadc843e19c1b5ae588ea41f
devankit01 <devankitgupta01@gmail.com>
2020-09-24 23:45:30 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+
 
 function App() {
+  
+  const [quote,setquote] = useState('I’v…over again in my life. And that is why I succeed.')
+  const [author,setauthor] = useState('Michael Jordan')
+
+
+  const GetQuote = () =>{
+  // const [quote,setquote] = useState('')
+    let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      let dataquotes = data.quotes;
+      let random = Math.floor(Math.random()*dataquotes.length);
+      let newquote = dataquotes[random];
+      console.log(newquote);
+      setquote(newquote.quote);
+      setauthor(newquote.author);
+      const arr = ['#66CC99','#16A085','#3A539B','#1F3A93','#2ABB9B','#F7CA18']
+      let ran = Math.floor(Math.random()*arr.length)
+      document.body.style.background = String(arr[ran]);
+     
+
+    })
+  }
+  const handleClick = ()  =>{
+    console.log('✅')
+    GetQuote();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+<p>{quote}</p>
+       <p>–{author}</p>
+       <button onClick={handleClick}>New Quote</button>
+      </div>
     </div>
   );
 }
